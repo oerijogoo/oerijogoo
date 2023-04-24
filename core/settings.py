19a9727d7fffbca8d8d95c670ebef585ff80d1db
oerijogoo <joerironald@gmail.com>
2023-04-24 15:17:01 +0300

    docker files

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zlx=a89ia+k_c&q@r_-$%h%11v1!j5+ove0%%z*vh(rzeola8h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
